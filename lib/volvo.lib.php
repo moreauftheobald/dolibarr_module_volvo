fb741874dc82669e39a9ee1133cb770b687896d3
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-09 13:19:21 +0100

    debug

--- lib/volvo.lib.php
+++ lib/volvo.lib.php
@@ -146,8 +146,7 @@ Function print_extra($key,$type,$action,$extrafields,$object){
 			$out.= '<input type="hidden" name="token" value="' . $_SESSION['newtoken'] . '">';
 			$out.= '<input type="hidden" name="id" value="' . $object->id . '">';
 			$list = $extrafields->attribute_param[$key]['options'];
-			var_dump($list);
-			//$out.= $form->select_withcheckbox_flat('options_'.$key,$list,$object->array_options['options_'.$key]);
+			$out.= $form->select_withcheckbox_flat('options_'.$key,$list,$object->array_options['options_'.$key]);
 			$out.= '<input type="submit" class="button" value="Modifier">';
 			$out.= '</form>';
 		} else {
