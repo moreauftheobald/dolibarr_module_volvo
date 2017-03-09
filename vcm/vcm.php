e35edd29bf01c9c4787647856e91a8c0242c828b
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-09 11:59:49 +0100

    debug

--- vcm/vcm.php
+++ vcm/vcm.php
@@ -46,6 +46,7 @@ if ($action == 'edit_extra' && GETPOST('attribute') =='vcm_deja') {
 	print '<input type="hidden" name="token" value="' . $_SESSION ['newtoken'] . '">';
 	print '<input type="hidden" name="action" value="update_extras">';
 	print '<input type="hidden" name="attribute" value="vcm_deja">';
+	print '<input type="hidden" name="id" value="' . $object->id . '">';
 	print $form->selectyesno('options_vcm_deja',$object->array_options['options_vcm_deja'],1);
 	print '<input type="submit" class="button" value="' . $langs->trans('Modify') . '">';
 	print '</form>';
