bea87af30f8ab9c8c2bf5880d340ccdb9cffc96d
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-10 15:32:22 +0100

    debug

--- commande/document.php
+++ commande/document.php
@@ -25,7 +25,7 @@
  *	\brief      Management page of documents attached to an order
  */
 
-require '../main.inc.php';
+require '../../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/volvo/lib/volvo.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/images.lib.php';
