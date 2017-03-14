3576619ab66bdd02e6cc9e260016099b3fa9bee5
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 14:09:44 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -226,7 +226,7 @@ class pdf_vcm extends ModelePDFContract
 				$extralabels = $extrafields->fetch_name_optionals_label($object->table_element, true);
 
 				$sys = new Leadext($this->db);
-				$y=array(25,37.3,49.5,61.8,65.8,70.2,97.2,103.3,107.8,112.3);
+				$y=array(25,37.3,49.5,61.8,65.8,70.2,97.2,103.3,107.8,112.3,118.2,122.7,127.2,131.7);
 
 // 				$x = $sys->prepare_array('VOLVO_ANALYSELG_X', 'array');
 // 				$z = $sys->prepare_array('VOLVO_ANALYSELG_Z', 'array');
@@ -322,6 +322,26 @@ class pdf_vcm extends ModelePDFContract
  				$pdf->SetFont('','', $default_font_size);
  				$pdf->SetXY(170.5, $y[9]);
  				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_pvc']));
+ 				$pdf->MultiCell(30, 0, $out,0,'L');
+
+ 				$pdf->SetFont('','', $default_font_size);
+ 				$pdf->SetXY(170.5, $y[10]);
+ 				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_blue']));
+ 				$pdf->MultiCell(30, 0, $out,0,'L');
+
+ 				$pdf->SetFont('','', $default_font_size);
+ 				$pdf->SetXY(170.5, $y[11]);
+ 				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_silver']));
+ 				$pdf->MultiCell(30, 0, $out,0,'L');
+
+ 				$pdf->SetFont('','', $default_font_size);
+ 				$pdf->SetXY(170.5, $y[12]);
+ 				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_silverp']));
+ 				$pdf->MultiCell(30, 0, $out,0,'L');
+
+ 				$pdf->SetFont('','', $default_font_size);
+ 				$pdf->SetXY(170.5, $y[13]);
+ 				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_gold']));
  				$pdf->MultiCell(30, 0, $out,0,'L');
 
 // 				//Carac client
