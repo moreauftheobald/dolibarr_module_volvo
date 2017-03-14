ef3b8fa83ee9fd166d68c9a95ee197f73bfc5d82
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 15:41:19 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -227,7 +227,7 @@ class pdf_vcm extends ModelePDFContract
 				$extralabels = $extrafields->fetch_name_optionals_label($object->table_element, true);
 
 				$sys = new Leadext($this->db);
-				$y=array(25.5,37.3,49.5,61.8,65.8,70.2,97.2,103.3,107.8,112.3,118.2,122.7,127.2,131.7,138.7,143,147.7);
+				$y=array(25.3,37.3,49.5,61.8,65.8,70.2,97.2,103.3,107.8,112.3,118.2,122.7,127.2,131.7,138.7,143,147.7);
 
 // 				$x = $sys->prepare_array('VOLVO_ANALYSELG_X', 'array');
 // 				$z = $sys->prepare_array('VOLVO_ANALYSELG_Z', 'array');
