79211d051c94880372f5b90318d80df811c92ff8
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 15:05:26 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -349,11 +349,11 @@ class pdf_vcm extends ModelePDFContract
  				$selected = explode(',', $object->array_options['options_vcm_duree']);
  				foreach ($extrafields->attribute_param['vcm_duree']['options'] as $key => $value){
  					if(in_array($key, $selected)){
- 						$pdf->image("http://www.erp-theobald.com" . show_picto_pdf(1),194,32,0,7);
- 						$pdf->MultiCell(30, 0, $value,0,'L');
+ 						$pdf->image("http://www.erp-theobald.com" . show_picto_pdf(1),194,32,7,7);
+ 						//$pdf->MultiCell(30, 0, $value,0,'L');
  					}else{
- 						$pdf->image("http://www.erp-theobald.com" . show_picto_pdf(0),194,32,0,7);
- 						$pdf->MultiCell(30, 0, $value,0,'L');
+ 						$pdf->image("http://www.erp-theobald.com" . show_picto_pdf(0),194,32,7,7);
+ 						//$pdf->MultiCell(30, 0, $value,0,'L');
  					}
  				}
 
