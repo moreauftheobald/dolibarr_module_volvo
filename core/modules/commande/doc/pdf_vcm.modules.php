b072225e7b2daa74fd7c63f024443262352ae070
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 16:24:41 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -443,7 +443,7 @@ class pdf_vcm extends ModelePDFContract
  					}
  				}
 
- 				$pdf->SetFont('','', $default_font_size-2);
+ 				$pdf->SetFont('','', $default_font_size);
  				$pdf->SetXY($x[0], $yp[8]);
  				$out = $outputlangs->convToOutputCharset('<B>Carrosserie et equipements: </b>' . $object->array_options['options_vcm_carr']);
  				$pdf->writeHTMLCell(194,20,8,172,$out);
