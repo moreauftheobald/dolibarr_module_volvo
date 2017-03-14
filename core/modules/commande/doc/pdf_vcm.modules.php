7b5e34f7c311b6feadeff65ab6eff2c405ca51cb
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 13:00:49 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -235,7 +235,7 @@ class pdf_vcm extends ModelePDFContract
 				$commercial->fetch($object->user_author_id);
 
 				$pdf->SetFont('','', $default_font_size-1);
-				$pdf->SetXY(38, 24.7);
+				$pdf->SetXY(38, 24.6);
 				$out = $outputlangs->convToOutputCharset($commercial->firstname . ' ' . $commercial->lastname);
 				$pdf->MultiCell($z[2]+$z[3], 0, $out,0,'L');
 
