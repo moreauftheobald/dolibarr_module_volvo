b8001ef10906f2b74a7255b4c07ae6729ae10d8f
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-14 14:01:10 +0100

    debug

--- core/modules/commande/doc/pdf_vcm.modules.php
+++ core/modules/commande/doc/pdf_vcm.modules.php
@@ -226,7 +226,7 @@ class pdf_vcm extends ModelePDFContract
 				$extralabels = $extrafields->fetch_name_optionals_label($object->table_element, true);
 
 				$sys = new Leadext($this->db);
-				$y=array(25,37.3,49.5,61.8,65.8,70.2);
+				$y=array(25,37.3,49.5,61.8,65.8,70.2,97.2);
 
 // 				$x = $sys->prepare_array('VOLVO_ANALYSELG_X', 'array');
 // 				$z = $sys->prepare_array('VOLVO_ANALYSELG_Z', 'array');
@@ -302,6 +302,11 @@ class pdf_vcm extends ModelePDFContract
  				$pdf->SetFont('','', $default_font_size);
  				$pdf->SetXY(173, $y[5]);
  				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_trf_dfol']));
+ 				$pdf->MultiCell(30, 0, $out,0,'L');
+
+ 				$pdf->SetFont('','', $default_font_size);
+ 				$pdf->SetXY(170, $y[6]);
+ 				$out = $outputlangs->convToOutputCharset(yn($object->array_options['options_vcm_ppc']));
  				$pdf->MultiCell(30, 0, $out,0,'L');
 
 // 				//Carac client
