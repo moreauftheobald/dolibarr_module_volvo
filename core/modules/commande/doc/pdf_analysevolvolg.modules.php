55309fe39fb0cf643a52f5da0e1bb7696d9235bd
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-03-20 17:43:07 +0100

    debug

--- core/modules/commande/doc/pdf_analysevolvolg.modules.php
+++ core/modules/commande/doc/pdf_analysevolvolg.modules.php
@@ -494,9 +494,9 @@ class pdf_analysevolvolg extends ModelePDFContract
 
 							$pdf->SetFont('','', $default_font_size);
 							$pdf->SetXY($x[5], $internspace[$intern]);
-							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht) . ' €');
+							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht * $line->qty) . ' €');
 							$pdf->MultiCell($z[5], 0, $out,0,'R');
-							$totalpa+=$line->pa_ht;
+							$totalpa+=$line->pa_ht * $line->qty;
 
 							if(!empty($line->array_options['options_fk_supplier'])){
 
@@ -509,7 +509,7 @@ class pdf_analysevolvolg extends ModelePDFContract
 								$ecart = $line->total_ht - $line->array_options['options_buyingprice_real'];
 
 							}else{
-								$ecart = $line->total_ht - $line->pa_ht;
+								$ecart = $line->total_ht - ($line->pa_ht * $line->qty);
 							}
 
 							if(!empty($ecart)){
@@ -538,9 +538,9 @@ class pdf_analysevolvolg extends ModelePDFContract
 
 							$pdf->SetFont('','', $default_font_size);
 							$pdf->SetXY($x[5], $externspace[$externe]);
-							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht) . ' €');
+							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht * $line->qty) . ' €');
 							$pdf->MultiCell($z[5], 0, $out,0,'R');
-							$totalpa+=$line->pa_ht;
+							$totalpa+=$line->pa_ht * $line->qty;
 
 							if(!empty($line->array_options['options_fk_supplier'])){
 
@@ -553,7 +553,7 @@ class pdf_analysevolvolg extends ModelePDFContract
 								$ecart = $line->total_ht - $line->array_options['options_buyingprice_real'];
 
 							}else{
-								$ecart = $line->total_ht - $line->pa_ht;
+								$ecart = $line->total_ht - ($line->pa_ht * $line->qty);
 							}
 
 							if(!empty($ecart)){
@@ -583,9 +583,9 @@ class pdf_analysevolvolg extends ModelePDFContract
 
 							$pdf->SetFont('','', $default_font_size);
 							$pdf->SetXY($x[5], $diverspace[$divers]);
-							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht) . ' €');
+							$out = $outputlangs->convToOutputCharset(Price($line->pa_ht * $line->qty) . ' €');
 							$pdf->MultiCell($z[5], 0, $out,0,'R');
-							$totalpa+=$line->pa_ht;
+							$totalpa+=$line->pa_ht * $line->qty;
 
 							if(!empty($line->array_options['options_fk_supplier'])){
 
@@ -598,7 +598,7 @@ class pdf_analysevolvolg extends ModelePDFContract
 								$ecart = $line->total_ht - $line->array_options['options_buyingprice_real'];
 
 							}else{
-								$ecart = $line->total_ht - $line->pa_ht;
+								$ecart = $line->total_ht - ($line->pa_ht * $line->qty);
 							}
 
 							if(!empty($ecart)){
