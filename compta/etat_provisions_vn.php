050c9f10b0f42e52244e0cc9c30838ee5b9ccc68
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-04 18:05:56 +0200

    debug

--- compta/etat_provisions_vn.php
+++ compta/etat_provisions_vn.php
@@ -149,105 +149,78 @@ if(!empty($search_periode)){
 $arrayresult1 = stat_prov1($year,$monthlist);
 
 
-// foreach ($arrayperiode as $m) {
-//  	$var = ! $var;
-//  	$total_fact+=$arrayresult1['nb_fact'][$m];
-// 	$total_caht+=$arrayresult1['catotalht'][$m];
-// 	$total_tracteur+=$arrayresult1['nbtracteur'][$m];
-// 	$total_porteur+=$arrayresult1['nbporteur'][$m];
-// 	$total_vcm+=$arrayresult2['vcm'][$m];
-// 	$total_dfol+=$arrayresult2['dfol'][$m];
-// 	$total_dded+=$arrayresult2['dded'][$m];
-// 	$total_vfs+=$arrayresult2['vfs'][$m];
-// 	$total_lixbail+=$arrayresult2['lixbail'][$m];
-// 	$total_cavolvo+=$arrayresult3['cavolvo'][$m];
-// 	$total_margetheo+=$arrayresult4['margetheo'][$m];
-// 	$total_margereal+=$arrayresult4['margereal'][$m];
+foreach ($arrayresult1 as $key=>$arrayvalue) {
+ 	$var = ! $var;
 
-//  	print '<tr ' . $bc[$var] . '>';
-// 	print '<td align="center">' . $month[$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult1['nb_fact'][$m] . '</td>';
-// 	if(!empty($arrayresult1['catotalht'][$m])){
-// 		print '<td align="center">'. price($arrayresult1['catotalht'][$m]) .' €</td>';
-// 	}else{
-// 		print '<td align="center"></td>';
-// 	}
-// 	if(!empty($arrayresult3['cavolvo'][$m])){
-// 		print '<td align="center">'. price($arrayresult3['cavolvo'][$m]) .' €</td>';
-// 	}else{
-// 		print '<td align="center"></td>';
-// 	}
-// 	print '<td align="center">' . $arrayresult1['nbtracteur'][$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult1['nbporteur'][$m] . '</td>';
-// 	if(!empty($arrayresult1['nb_fact'][$m])){
-// 		$tracteur_percent = round(($arrayresult1['nbtracteur'][$m] /($arrayresult1['nb_fact'][$m]))*100,2) . ' %';
-// 		$porteur_percent = round(($arrayresult1['nbporteur'][$m] /($arrayresult1['nb_fact'][$m]))*100,2) . ' %';
-// 	}else{
-// 		$tracteur_percent = '';
-// 		$porteur_percent = '';
-// 	}
-// 	print '<td align="center">' . $porteur_percent . '</td>';
-// 	print '<td align="center">' . $tracteur_percent . '</td>';
-// 	print '<td align="center">' . $arrayresult2['vcm'][$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult2['dfol'][$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult2['dded'][$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult2['vfs'][$m] . '</td>';
-// 	print '<td align="center">' . $arrayresult2['lixbail'][$m] . '</td>';
-// 	if(!empty($arrayresult4['margetheo'][$m])){
-// 		print '<td align="center">'. price($arrayresult4['margetheo'][$m]) .' €</td>';
-// 		print '<td align="center">'. price(round($arrayresult4['margetheo'][$m]/$arrayresult1['nb_fact'][$m],2)) .' €</td>';
-// 	}else{
-// 		print '<td align="center"></td>';
-// 		print '<td align="center"></td>';
-// 	}
-// 	if(!empty($arrayresult4['margereal'][$m])){
-// 		print '<td align="center">'. price($arrayresult4['margereal'][$m]) .' €</td>';
-// 		print '<td align="center">'. price(round($arrayresult4['margereal'][$m]/$arrayresult1['nb_fact'][$m],2)) .' €</td>';
-// 	}else{
-// 		print '<td align="center"></td>';
-// 		print '<td align="center"></td>';
-// 	}
-// 	if(!empty($arrayresult4['margetheo'][$m]) && !empty($arrayresult4['margereal'][$m])){
-// 		print '<td align="center">'. price($arrayresult4['margereal'][$m]-$arrayresult4['margetheo'][$m]) .' €</td>';
-// 		print '<td align="center">'. price(round(($arrayresult4['margereal'][$m]-$arrayresult4['margetheo'][$m])/$arrayresult1['nb_fact'][$m],2)) .' €</td>';
-// 	}else{
-// 		print '<td align="center"></td>';
-// 		print '<td align="center"></td>';
-// 	}
+ 	print '<tr ' . $bc[$var] . '>';
+	print '<td align="center">' . $key . '</td>';
+	print '<td align="center">' . $value['vendeur'] . '</td>';
+	print '<td align="center">' . $value['client'] .' €</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
+	print '<td align="center">' . '' . '</td>';
 
-// 	print "</tr>\n";
+	print "</tr>\n";
 
-// }
+}
 
-// print '<tr class="liste_titre">';
-// print '<th class="liste_titre" align="center">Total</th>';
-// print '<th class="liste_titre" align="center">' . $total_fact . '</th>';
-// print '<th class="liste_titre" align="center">'. price($total_caht) .' €</th>';
-// print '<th class="liste_titre" align="center">'. price($total_cavolvo) .' €</th>';
-// print '<th class="liste_titre" align="center">' . $total_tracteur . '</th>';
-// print '<th class="liste_titre" align="center">' . $total_porteur . '</th>';
-// if(!empty($total_fact)){
-// 	$tracteur_percent = round(($total_tracteur /($total_fact))*100,2);
-// 	$porteur_percent = round(($total_porteur /($total_fact))*100,2);
-// }else{
-// 	$tracteur_percent = 0;
-// 	$porteur_percent = 0;
-// }
-// print '<th class="liste_titre" align="center">' . price($tracteur_percent) . ' %</th>';
-// print '<th class="liste_titre" align="center">' . price($tracteur_percent) . ' %</th>';
-// print '<th class="liste_titre" align="center">' . $total_vcm . '</th>';
-// print '<th class="liste_titre" align="center">' . $total_dfol . '</th>';
-// print '<th class="liste_titre" align="center">' . $total_dded . '</th>';
-// print '<th class="liste_titre" align="center">' . $total_vfs . '</th>';
-// print '<th class="liste_titre" align="center">' . $total_lixbail . '</th>';
-// print '<th class="liste_titre" align="center">' . price($total_margetheo) . ' €</th>';
-// print '<th class="liste_titre" align="center">' . price(round($total_margetheo/$total_fact,2)) . ' €</th>';
-// print '<th class="liste_titre" align="center">' . price($total_margereal) . '</th>';
-// print '<th class="liste_titre" align="center">' . price(round($total_margereal/$total_fact,2)) . '</th>';
-// print '<th class="liste_titre" align="center">' . price($total_margereal-$total_margetheo) . '</th>';
-// print '<th class="liste_titre" align="center">' . price(round(($total_margereal-$total_margetheo)/$total_fact,2)) . '</th>';
+print '<tr class="liste_titre">';
+print '<th class="liste_titre" align="center">Total</th>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
+print '<td align="center">' . '' . '</td>';
 
-// print "</tr>\n";
+print "</tr>\n";
 
 
 print "</table>";
