fcc8a3408c70d8bc9b7df360eec97d520568ae60
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-19 17:13:30 +0200

    debug

--- lead/lead_portfolio.php
+++ lead/lead_portfolio.php
@@ -486,9 +486,9 @@ print '<input type="hidden" name="ordercreatedid" id="ordercreatedid" />';
 // }
 
 
-function allowDrop(ev) {
-	ev.preventDefault();
-}
+// function allowDrop(ev) {
+// 	ev.preventDefault();
+// }
 
 function drag(ev) {
 	ev.dataTransfer.setData("text", ev.target.id);
