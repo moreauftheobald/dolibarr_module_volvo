c34e5f2382f25b8b43b479477fd4c8c164dd8e3a
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-26 10:29:11 +0200

    debug

--- class/table_template.php
+++ class/table_template.php
@@ -1,6 +1,11 @@
 <?php
-global $test;
+global $test,$title;
 echo $test;
+
+$limit = GETPOST("limit")?GETPOST("limit","int"):$conf->liste_limit;
+$sortfield = GETPOST("sortfield",'alpha');
+$sortorder = GETPOST("sortorder",'alpha');
+$page = GETPOST("page",'int');
 
 print_barre_liste($title, $page, $_SERVER['PHP_SELF'], $option, $sortfield, $sortorder, '', $num, $nbtotalofrecords);
 ?>
