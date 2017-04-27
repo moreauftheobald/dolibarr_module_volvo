d0f0d24dd0e5a7c8e9fdd77fc01330bbeae878c5
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-27 11:50:12 +0200

    debug

--- lib/volvo.lib.php
+++ lib/volvo.lib.php
@@ -711,7 +711,7 @@ function stat_sell5($year, $commercial,$monthlist,$mode='GROUP'){
 				$result[$obj->ref]['id'] = $obj->id;
 			}
 		}
-		return $sql;
+		return $result;
 	}else{
 		return $sql;
 	}
