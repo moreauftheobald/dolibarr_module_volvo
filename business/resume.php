b2eb43f5e79775660d774addfa83e4604d50a359
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-04-27 11:48:44 +0200

    debug

--- business/resume.php
+++ business/resume.php
@@ -110,7 +110,7 @@ $arrayresult3 = stat_sell3($year, $search_commercial,$monthlist);
 $arrayresult4 = stat_sell4($year, $search_commercial,$monthlist);
 $arrayresult5 = stat_sell5($year, $search_commercial,$monthlist);
 
-print_r $arrayresult5;
+print_r($arrayresult5);
 
 $array_display=array();
 
