6f963bea79a3a8c59cd81dc04a8864cd2766c716
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-02 16:21:54 +0200

    debug

--- orders/createorder-new.php
+++ orders/createorder-new.php
@@ -138,8 +138,8 @@ if ($resql) {
 }
 $internesection='';
 foreach ($interne as $key=>$array){
-	$internesection.= '<div class="cal_event cal_event_busy" align="left" id="fixe_'. $key . '" style="background:' . $color .'; ';
-	$internesection.= 'background: -webkit-gradient(linear, left top, left bottom, from('.$color.'), to('.$color2.')); ';
+	$internesection.= '<div class="cal_event cal_event_busy" align="left" id="fixe_'. $key . '" style="background:#cccccc; ';
+	$internesection.= 'background: -webkit-gradient(linear, left top, left bottom, from(#cccccc), to(#b2b2b2)); ';
 	$internesection.= 'border-radius:6px; margin-bottom: 3px;">';
 	$internesection.= '<h><a href="" onclick="javascript:visibilite(\'' . $key . '\'); return false;" >'. img_edit_add('+','') . '</a>' . $key . '</h>';
 	$internesection.= '<div id="' . $key . '" style="display:none;">';
