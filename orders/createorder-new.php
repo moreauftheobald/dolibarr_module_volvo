308f1bff8defc37e76e3b03d7b8cf567bc4dbdf2
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-02 16:26:04 +0200

    debug

--- orders/createorder-new.php
+++ orders/createorder-new.php
@@ -141,7 +141,7 @@ foreach ($interne as $key=>$array){
 	$internesection.= '<div class="cal_event cal_event_busy" align="left" id="fixe_'. $key . '" style="background:#cccccc; ';
 	$internesection.= 'background: -webkit-gradient(linear, left top, left bottom, from(#cccccc), to(#b2b2b2)); ';
 	$internesection.= 'border-radius:6px; margin-bottom: 3px;">';
-	$internesection.= '<h><a href="" onclick="javascript:visibilite(\'' . $key . '\'); return false;" >'. img_edit_add('+','') . '</a>' . $key . '</h>';
+	$internesection.= '<h style="font-size: large;><a href="" onclick="javascript:visibilite(\'' . $key . '\'); return false;" >'. img_edit_add('+','') . '<b></a> ' . $key . ' </b></h>';
 	$internesection.= '<div id="' . $key . '" style="display:none;">';
 	$internesection.= $formvolvo->select_withcheckbox("interne_".$key,$array);
 	$internesection.= '</div>';
