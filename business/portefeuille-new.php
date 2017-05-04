5737169d53b4c117b1d1115800901766c5a273f9
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-04 16:20:59 +0200

    debug

---
+++ business/portefeuille-new.php
@@ -0,0 +1,241 @@
+<?php
+/*
+ * Copyright (C) 2014 Florian HENRY <florian.henry@open-concept.pro>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$res = @include '../../main.inc.php'; // For root directory
+if (! $res)
+	$res = @include '../../../main.inc.php'; // For "custom" directory
+if (! $res)
+	die("Include of main fails");
+
+require_once DOL_DOCUMENT_ROOT . '/volvo/class/lead.extend.class.php';
+require_once DOL_DOCUMENT_ROOT . '/volvo/class/table_template.class.php';
+
+$object = new Leadext($db);
+
+
+
+// Security check
+if (! $user->rights->volvo->port)
+	accessforbidden();
+
+
+$table = new Dyntable($db);
+
+$table->title = 'Suivis d\'activité VN volvo';
+
+$field= new Dyntable_fields($db);
+$field->name='comm';
+$field->label = 'Commercial';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'comm';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='dossier';
+$field->label = 'Dossier';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'com.ref';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='om';
+$field->label = 'N° O.M.';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'ef.numom';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='client';
+$field->label = 'Client';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'socnom';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='dt_cmd';
+$field->label = 'Date de Commande';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'cf.date_commande';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='dt_liv';
+$field->label = 'Date de livraison';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'com.date_livraison';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='dt_liv_usi';
+$field->label = 'Date de sortie d\'usine';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'dt_sortie';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='vin';
+$field->label = 'N° de Chassis';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'ef.vin';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='mois';
+$field->label = 'Mois';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->field = 'dt_sortie';
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='genre';
+$field->label = 'genre';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='type';
+$field->label = 'type';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='sil';
+$field->label = 'sil';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$field= new Dyntable_fields($db);
+$field->name='pv';
+$field->label = 'Prix de vente';
+$field->checked = 1;
+$field->sub_title = 0;
+$field->align = 'center';
+$table->arrayfields[$field->name] = $field;
+
+$tools =array();
+
+$tool = new Dyntable_tools($db);
+$tool->type = 'select_year';
+$tool->title = 'Année: ';
+$tool->value = $year;
+$tool->html_name = 'year';
+$tool->use_empty = 0;
+$tool->min_year = 5;
+$tool->max_year = 0;
+$tool->default = dol_print_date(dol_now(),'%Y');
+$tool->filter = 'YEAR_IN';
+$tools['1'] = $tool;
+
+$tool = new Dyntable_tools($db);
+$tool->type = 'select_user';
+$tool->title = 'Commercial: ';
+$tool->value = $search_commercial;
+$tool->html_name = 'search_commercial';
+$tool->use_empty = 1;
+$tool->see_all = $user->rights->volvo->stat_all;
+$tool->limit_to_group = '1';
+$tool->filter = 'lead.fk_user_resp';
+$tools['2'] = $tool;
+
+$periodarray= array(
+	1 => 'Janvier',
+	2 => 'Fevrier',
+	3 => 'Mars',
+	4 => 'Avril',
+	5=> 'Mai',
+	6=> 'Juin',
+	7 => 'Juillet',
+	8 => 'Aout',
+	9 => 'Septembre',
+	10 => 'Octobre',
+	11 => 'Novembre',
+	12 => 'Décembre',
+	13=>'1er Trimestre',
+	14=> '2eme Trimestre',
+	15=>'3eme Trimestre',
+	16=>'4eme Trimestre',
+	17=>'1er Semestre',
+	18=>'2eme Semestre'
+);
+
+$tool = new Dyntable_tools($db);
+$tool->type = 'select_array';
+$tool->title = 'Periode: ';
+$tool->value = $search_periode;
+$tool->html_name = 'search_periode';
+$tool->use_empty = 1;
+$tool->array = $periodarray;
+$tool->value = $search_periode;
+$tool->filter = 'MONTH_IN';
+$tools['3'] = $tool;
+
+$table->extra_tools =$tools;
+$table->sortorder = GETPOST('sortorder', 'alpha');
+$table->sortfield = GETPOST('sortfield', 'alpha');
+$table->page = GETPOST('page', 'int');
+
+$table->offset = ($conf->liste_limit+1) * $page;
+
+if (empty($table->sortorder))
+	$table->sortorder = "ASC";
+if (empty($sortfield))
+	$table->sortfield = "dt_sortie";
+
+$table->export_name = 'portefeuille';
+$table->context = 'portefeuille';
+$table->filter = array();
+$table->filter['PORT'] = 1;
+
+$table->header();
+
+$table->draw_tool_bar();
+
+$table->draw_table_head();
+
+
+
+
+
+
+
+
+
---
+++ class/table_template.class.php
@@ -0,0 +1,335 @@
+<?php
+global $list_config,$conf,$db;
+
+require_once DOL_DOCUMENT_ROOT . '/core/class/html.formother.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/html.form.class.php';
+require_once DOL_DOCUMENT_ROOT . '/user/class/user.class.php';
+
+class Dyntable
+{
+	public $arrayfields = array();
+	public $title;
+	public $extra_tools=array();
+	public $sortorder;
+	public $sortfield;
+	public $page;
+	public $offset;
+	public $filter = array();
+	public $nbtotalofrecords;
+	public $array_display = array();
+	public $search_button;
+	public $remove_filter_button;
+	public $export_button;
+	public $select_fields_button;
+	public $num;
+	public $option;
+	public $tools_active;
+	public $export_name;
+	public $context;
+	public $sub_title = array();
+
+	function __construct($db)
+	{
+		$this->db = $db;
+
+	}
+
+	function header(){
+		llxHeader('', $this->title);
+		print_barre_liste($this->title, $this->page, $_SERVER['PHP_SELF'], $this->option, $this->sortfield, $this->sortorder, '', $this->num, $this->nbtotalofrecords);
+	}
+
+	function draw_tool_bar(){
+		global $conf;
+
+		print '<form method="post" action="' . $_SERVER['PHP_SELF'] . '" name="search_form">' . "\n";
+		print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+		print '<input type="hidden" name="formfilteraction" id="formfilteraction" value="list">';
+		print '<input type="hidden" name="sortfield" value="'.$this->sortfield.'">';
+		print '<input type="hidden" name="sortorder" value="'. $this->sortorder.'">';
+		print '<table class="noborder" width="100%">';
+		print '<tr class="liste_titre" style="height:22px;">';
+		print '<th class="liste_titre" align="center" style="white-space:nowrap; width:90px;">';
+		if($this->search_button==1){
+			print '<input class="liste_titre" type="image" src="' . DOL_URL_ROOT . '/theme/' . $conf->theme . '/img/search.png" value="Search" title="Search">';
+		}
+		if($this->remove_filter_button==1){
+			print '&nbsp;<input type="image" class="liste_titre" name="button_removefilter" src="' . DOL_URL_ROOT . '/theme/' . $conf->theme . '/img/searchclear.png" value="RemoveFilter" title="RemoveFilter">';
+		}
+		if($this->export_button==1){
+			print '&nbsp;<input type="image" class="liste_titre" name="button_export" src="' . DOL_URL_ROOT . '/theme/common/mime/xls.png" value="export" title="Exporter" width="16px" height="16px">';
+		}
+		print '</th>';
+		if($this->select_fields_button==1){
+			print '<th class="liste_titre" align="center" style="white-space:nowrap; width:40px;">';
+			print $selectfields;
+			print '</th>';
+		}
+
+		if(is_array($this->extra_tools)){
+			foreach ($this->extra_tools as $key => $p){
+				print '<th class="liste_titre" align="left" style="white-space:nowrap; width:1%;">';
+					$p->draw_tool;
+				print '</th>';
+			}
+		}
+		print '<th class="liste_titre"></th>';
+		print "</tr>";
+		print '</table>';
+		print '</form>';
+
+	}
+
+	function draw_table_head(){
+
+		$group= array();
+		foreach ($this->arrayfields as $f){
+			if($f->checked==1) $group[$f->sub_title]+=1;
+		}
+
+		print '<table class="noborder" width="100%">';
+		if(is_array($this->sub_title)){
+			print '<tr class="liste_titre" style="height:22px;">';
+			foreach ($this->arrayfields as $f){
+				if($f->sub_title>0 && $f->checked == 1 && $groupdone[$f->sub_title]==0){
+					print '<th class="liste_titre" colspan="' . $group[$f->sub_title] . '" align="center">' . $this->sub_title[$f->sub_title] . '</th>';
+					$groupdone[$f->sub_title] = 1;
+				}elseif($f->sub_title==0 && $f->checked == 1){
+					print_liste_field_titre($f->label,$_SERVER["PHP_SELF"],$f->field,'',$this->option,'rowspan="2" align="' . $f->align .'"',$this->sortfield,$this->sortorder);
+				}
+			}
+			print "</tr>";
+			print '<tr class="liste_titre" style="height:22px;">';
+			foreach ($this->arrayfields as $f){
+				if($f->sub_title>0 && $f->checked == 1){
+					print_liste_field_titre($f->label,$_SERVER["PHP_SELF"],$f->field,'',$this->option,'align="' . $f->align .'"',$this->sortfield,$this->sortorder);
+				}
+			}
+			print "</tr>";
+		}else{
+			print '<tr class="liste_titre" style="height:22px;">';
+			foreach ($this->arrayfields as $f){
+				if($f->checked == 1){
+					print_liste_field_titre($f->label,$_SERVER["PHP_SELF"],$f->field,'',$this->option,'align="' . $f->align .'"',$this->sortfield,$this->sortorder);
+				}
+			}
+			print "</tr>";
+		}
+
+	}
+
+	function draw_data(){
+
+		foreach ($this->array_data as $l){
+			print '<tr ' . $l['class'] . '>';
+			$td_class = $l['class_td'];
+			foreach ($l as $key => $val){
+				if($this->array_fields[$key]->checked==1){
+					print '<td ' . $td_class . ' align="' . $this->array_fields[$key]->align . '" style="white-space:nowrap;">';
+					if(!empty($val)) print $val .' ' . $this->array_fields[$key]->unit;
+					print '</td>';
+				}
+			}
+			print '</tr>';
+		}
+
+	}
+
+	function end_table(){
+		print '</table>';
+
+		// footer
+		llxFooter();
+		$db->close();
+	}
+}
+
+
+
+class Dyntable_tools
+{
+	public $type;
+	public $title;
+	public $value;
+	public $html_name;
+	public $use_empty;
+	public $min_year;
+	public $max_year;
+	public $default;
+	public $filter;
+	public $see_all;
+	public $limit_to_group;
+	public $exclude_group;
+	public $array;
+
+	function __construct($db)
+	{
+		$this->db = $db;
+
+	}
+
+	function draw_tool(){
+	global $user;
+		switch($this->type){
+			case 'select_user':
+				if($this->value == -1) $this->value="";
+
+				if($this->see_all!=1){
+					$this->value = $user->id;
+					$disabled = 1;
+				}
+
+				if(!empty($this->limit_to_group)){
+					$user_included=array();
+					$sqlusers = "SELECT fk_user FROM " . MAIN_DB_PREFIX . "usergroup_user WHERE fk_usergroup IN(" . $this->limit_to_group . ") ";
+					$resqlusers  = $this->db->query($sqlusers);
+					if($resqlusers){
+						while ($users = $this->db->fetch_object($resqlusers)){
+							$user_included[] = $users->fk_user;
+						}
+					}
+				}
+
+				if(!empty($this->exclude_group)){
+					$user_excluded=array();
+					$sqlusers = "SELECT fk_user FROM " . MAIN_DB_PREFIX . "usergroup_user WHERE fk_usergroup IN(" . $this->exclude_group . ") ";
+					$resqlusers  = $db->query($sqlusers);
+					if($resqlusers){
+						while ($users = $db->fetch_object($resqlusers)){
+							$user_excluded[] = $users->fk_user;
+						}
+					}
+				}
+				print '&nbsp; &nbsp;' . $this->title;
+				print $form->select_dolusers($this->value,$this->html_name,$this->use_empty,$user_excluded,$disabled,$user_included);
+				break;
+
+			case 'select_year':
+				if($this->value == -1) $this->value="";
+
+				if(empty($this->value) && !empty($this->default)){
+					$this->value = $this->default;
+				}
+				print '&nbsp; &nbsp;' . $this->title;
+				$formother->select_year($this->value,$this->html_name,$this->use_empty,$this->min_year,$this->max_year);
+				break;
+
+			case 'select_array':
+						if($this->value == -1) $this->value="";
+						print '&nbsp; &nbsp;' . $this->title;
+						print $form->selectarray($this->html_name, $this->array,$this->value,$this->use_empty);
+				}
+	}
+
+
+}
+
+
+
+class Dyntable_fields
+{
+	public $name;
+	public $label;
+	public $checked;
+	public $sub_title;
+	public $field;
+	public $unit;
+	public $align;
+
+	function __construct($db)
+	{
+		$this->db = $db;
+
+	}
+}
+
+
+
+$form = new Form($db);
+$formother = new FormOther($db);
+
+$limit = GETPOST("limit")?GETPOST("limit","int"):$conf->liste_limit;
+$varpage=$list_config['context'];
+if (GETPOST('formfilteraction') == 'listafterchangingselectedfields')
+{
+	$tabparam=array();
+
+	if (GETPOST("selectedfields")) $tabparam["MAIN_SELECTEDFIELDS_".$varpage]=GETPOST("selectedfields");
+	else $tabparam["MAIN_SELECTEDFIELDS_".$varpage]='';
+
+	include_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
+
+	$result=dol_set_user_param($db, $conf, $user, $tabparam);
+}
+
+$selectfields = $form->multiSelectArrayWithCheckbox('selectedfields', $list_config['array_fields'], $varpage);
+
+if(GETPOST("button_export_x")){
+	$handler = fopen("php://output", "w");
+	header('Content-Type: text/csv');
+	header('Content-Disposition: attachment;filename=' . $list_config['export_name'] . '.csv');
+	fputs($handler, $bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ));
+
+	$commercial = new user($db);
+	if(!empty($search_commercial)){
+		$commercial->fetch($search_commercial);
+		$com = $commercial->firstname . ' ' . $commercial->lastname;
+	}
+
+	if(!empty($search_periode)){
+		if($search_periode == 1){
+			$periode = '1er Trimestre';
+		}elseif($search_periode==2){
+			$periode = '2eme Trimestre';
+		}elseif($search_periode==3){
+			$periode = '3eme Trimestre';
+		}elseif($search_periode==4){
+			$periode = '4eme Trimestre';
+		}elseif($search_periode==5){
+			$periode = '1er Semestre';
+		}elseif($search_periode==6){
+			$periode = '2eme Semestre';
+		}
+	}
+
+	$h=array(
+			'Année:',
+			$year,
+			'',
+			'commercial:',
+			$com,
+			'',
+			'Periode:',
+			$periode
+	);
+	fputcsv($handler, $h, ';', '"');
+
+	$h=array();
+
+	foreach ($list_config['array_fields'] as $f){
+		if($f['checked']==1) $h[] = $f['label'];
+	}
+	fputcsv($handler, $h, ';', '"');
+
+	foreach ($list_config['array_data'] as $d) {
+		$ligne=array();
+		foreach ($d as $key=>$val){
+			if($list_config['array_fields'][$key]['checked']==1) $ligne[] = strip_tags($val);
+		}
+		fputcsv($handler, $ligne, ';', '"');
+	}
+
+	exit;
+}
+
+
+
+
+
+
+//var_dump($group);
+
+
+
+
+?>
