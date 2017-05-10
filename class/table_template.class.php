1f31b78b302d28f0ca50e0eaa28df5c7804cc6ed
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-10 16:48:46 +0200

    debug

--- class/table_template.class.php
+++ class/table_template.class.php
@@ -490,7 +490,7 @@ class Dyntable
 						$name2 = $p->html_name .'max_';
 						$post1 = GETPOST($name1);
 						$post2 = GETPOST($name2);
-						if(!empty($post1) && !empty($post2)){
+						if(isset($post1) && isset($post2)){
 							$val1 = dol_mktime(0, 0, 0, GETPOST($name1.'month'), GETPOST($name1.'day'), GETPOST($name1.'year'));
 							$val2 = dol_mktime(0, 0, 0, GETPOST($name2.'month'), GETPOST($name2.'day'), GETPOST($name2.'year'));
 							$p->value = array($val1,$val2);
