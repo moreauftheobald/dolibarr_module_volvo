65ee1a1f29380d37fab1b2c573dd456ba517f08a
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-10 16:48:03 +0200

    debug

--- class/table_template.class.php
+++ class/table_template.class.php
@@ -448,7 +448,9 @@ class Dyntable
 				$name2 = $p->html_name .'max';
 				$post1 = GETPOST($name1);
 				$post2 = GETPOST($name2);
-				if(!empty($post1) && !empty($post2)){
+				if(isset($post1) || isset($post2)){
+					if(empty($post1)) $post1 = '0';
+					if(empty($post2)) $post2 = '0';
 					$p->value = array($post1,$post2);
 					$this->extra_tools[$key] = $p;
 					$this->filter[$p->filter] ="'" . $post1 . "' AND '" . $post2 ."'";
@@ -502,7 +504,9 @@ class Dyntable
 						$name2 = $p->html_name .'max';
 						$post1 = GETPOST($name1);
 						$post2 = GETPOST($name2);
-						if(!empty($post1) && !empty($post2)){
+						if(!empty($post1) || !empty($post2)){
+							if(empty($post1)) $post1 = '0';
+							if(empty($post2)) $post2 = '0';
 							$p->value = array($post1,$post2);
 							$this->arrayfields[$key]->filter[$keyfilter] = $p;
 							$this->filter[$p->filter] ="'" . $post1 . "' AND '" . $post2 ."'";
