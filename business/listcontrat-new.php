a04030e6aff22d37cb1e839b9e9ea01bd36bb5f0
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-12 12:55:36 +0200

    debug

--- business/listcontrat-new.php
+++ business/listcontrat-new.php
@@ -350,8 +350,6 @@ $table->data_array();
 
 $table->header();
 
-var_dump($table->sql);
-
 if ($action == 'set_date') {
 	$form = new Form($db);
 	$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"] . '?id='. $id . '&element=' . $element . $param, "Valider et passer a l'étape suivante", '', 'confirm_set_date', array(array(
