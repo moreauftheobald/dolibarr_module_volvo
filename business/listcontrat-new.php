e5b1bbdb5efea4754b586f2ba3453a1a7005c690
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-12 12:34:04 +0200

    debug

--- business/listcontrat-new.php
+++ business/listcontrat-new.php
@@ -322,8 +322,8 @@ $table->sql_from.= "LEFT JOIN " . MAIN_DB_PREFIX . "contrat_extrafields AS ef ON
 
 $table->sql_where = 'c.entity IN ('.getEntity('contract', 1).')';
 $table->sql_filter_action = array();
-$table->sql_filter_action[] = array('keys'=>array('s.nom','c.ref_supplier'), 'action' =>"#KEY# LIKE '%#VALUE#%'");
-$table->sql_filter_action[] = array('keys'=>array('c.rowid','s.rowid'), 'action' =>"#KEY# = #VALUE#");
+$table->sql_filter_action[] = array('keys'=>array('s.nom','c.ref_supplier','c.ref_customer'), 'action' =>"#KEY# LIKE '%#VALUE#%'");
+$table->sql_filter_action[] = array('keys'=>array('c.rowid','s.rowid','u.rowid'), 'action' =>"#KEY# = #VALUE#");
 
 
 /*
