3aab2510004b2999d8f81122a55c7bc95aeeb283
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-05-12 18:08:38 +0200

    debug

--- class/table_template.class.php
+++ class/table_template.class.php
@@ -546,10 +546,10 @@ class Dyntable
 		if (empty($this->sortfield))
 			$this->sortfield = $this->default_sortfield;
 
-		if(!empty($this->sortorder)) $this->option = '&sortorder='.$this->sortorder;
-		if(!empty($this->sortfield)) $this->option = '&sortfield='.$this->sortfield;
-		if(!empty($this->page)) $this->option = '&page='.$this->page;
-		if(!empty($this->offset)) $this->option = '&offset='.$this->offset;
+		$this->option = '&sortorder='.$this->sortorder;
+		$this->option = '&sortfield='.$this->sortfield;
+		$this->option = '&page='.$this->page;
+		$this->option = '&offset='.$this->offset;
 
 		if (GETPOST("button_removefilter_x")) {
 			foreach ($this->extra_tools as $key => $p){
