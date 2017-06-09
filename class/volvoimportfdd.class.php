a8487f035470648fe8bdbb16aadf2f64b4f63e37
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-06-09 11:27:05 +0200

    debug

--- class/volvoimportfdd.class.php
+++ class/volvoimportfdd.class.php
@@ -299,7 +299,7 @@ class VolvoImportfdd extends VolvoImport
 		dol_syslog(get_class($this) . '::' . __METHOD__, LOG_DEBUG);
 		$arrayresult = array();
 		foreach ($this->targetInfoArray as $info){
-			$arrayresult[]= $this->objWorksheet->getCell($info['cell'])->getCalculatedValue();
+			$arrayresult[]= $this->objWorksheet->getCell($info['cell'])->getValue();
 		}
 
 		var_dump($arrayresult);
