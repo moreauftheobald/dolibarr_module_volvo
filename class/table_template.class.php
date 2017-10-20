cadc525c51ad706432ea51140bc64b601cce7f6c
moreauf <moreauf@WGTPO57.theobald-groupe.local>
2017-10-20 17:56:21 +0200

    debug

--- class/table_template.class.php
+++ class/table_template.class.php
@@ -88,7 +88,7 @@ class Dyntable
 			}
 			if ($val->label)
 			{
-				$lis.='<li><input type="checkbox" value="'.$key.'"'.(empty($val->checked)?'':' checked="checked"').'/>'.dol_escape_htmltag($val->label).'</li>';
+				$lis.='<li><input type="checkbox" value="'.$key.'"'.(empty($val->checked)?'':' checked="checked"').'/>'.strip_tags($val->label).'</li>';
 				$listcheckedstring.=(empty($val->checked)?'':$key.',');
 			}
 		}
