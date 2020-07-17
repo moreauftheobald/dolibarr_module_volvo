9031c889547821d2a93e43d14ce1c501b94ca9f6
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-07-17 11:36:47 +0200

    d

--- core/modules/supplier_order/pdf/pdf_gop.modules.php
+++ core/modules/supplier_order/pdf/pdf_gop.modules.php
@@ -260,7 +260,7 @@ class pdf_gop extends ModelePDFSuppliersOrders
 
   				$pdf->SetFont('','', $default_font_size);
   				$pdf->SetXY(40, 137.6);
-  				$out = $outputlangs->convToOutputCharset($object->Lines['0']->desc);
+  				$out = $outputlangs->convToOutputCharset($object->Lines['1']->desc);
   				$pdf->MultiCell(150, 16.5, $out,0,'L');
 
 //  				$pdf->SetFont('','', $default_font_size);
