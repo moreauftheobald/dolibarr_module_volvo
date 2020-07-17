2858df3ef75d27db1d0892e48bb42358e24a2aa7
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-07-17 14:34:25 +0200

    d

--- core/modules/supplier_order/pdf/pdf_gop.modules.php
+++ core/modules/supplier_order/pdf/pdf_gop.modules.php
@@ -257,7 +257,7 @@ class pdf_gop extends ModelePDFSuppliersOrders
   				$pdf->writeHTML ($out);
 
   				$pdf->SetFont('','', $default_font_size);
-  				$pdf->SetXY(126, 196);
+  				$pdf->SetXY(126.5, 196.1);
   				if(!empty($object->cond_reglement_id)){
   					$text = $object->cond_reglement;
   				}else{
