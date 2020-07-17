2f13bd99658cebcd4bef5d1cae34eb1a152b5709
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-07-17 09:51:05 +0200

    v

--- core/modules/supplier_order/pdf/pdf_gop.modules.php
+++ core/modules/supplier_order/pdf/pdf_gop.modules.php
@@ -171,7 +171,7 @@ class pdf_gop extends ModelePDFSuppliersOrders
 				$reshook=$hookmanager->executeHooks('beforePDFCreation',$parameters,$object,$action);    // Note that $action and $object may have been modified by some hooks
 
                 $pdf=pdf_getInstance($this->format);
-                $default_font_size = pdf_getPDFFontSize($outputlangs)-1.5;	// Must be after pdf_getInstance
+                $default_font_size = pdf_getPDFFontSize($outputlangs)-1.3;	// Must be after pdf_getInstance
                 $heightforinfotot = 50;	// Height reserved to output the info and total part
 		        $heightforfreetext= (isset($conf->global->MAIN_PDF_FREETEXT_HEIGHT)?$conf->global->MAIN_PDF_FREETEXT_HEIGHT:5);	// Height reserved to output the free text on last page
 	            $heightforfooter = $this->marge_basse + 8;	// Height reserved to output the footer (value include bottom margin)
@@ -219,7 +219,7 @@ class pdf_gop extends ModelePDFSuppliersOrders
 
 
  				$pdf->SetFont('','', $default_font_size);
- 				$pdf->SetXY(73.4, 59.1);
+ 				$pdf->SetXY(73.5, 59.1);
  				$out = $outputlangs->convToOutputCharset(dol_print_date($object->date_valid,'daytext'));
  				$pdf->MultiCell(80, 0, $out,0,'L');
 
