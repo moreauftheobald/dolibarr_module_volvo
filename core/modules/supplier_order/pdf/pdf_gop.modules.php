e3a570d16f5ce37ff67ec453c8469f4f60608532
moreauf <moreauf@NGT15.theobald-groupe.local>
2020-07-17 14:48:25 +0200

    d

--- core/modules/supplier_order/pdf/pdf_gop.modules.php
+++ core/modules/supplier_order/pdf/pdf_gop.modules.php
@@ -247,17 +247,17 @@ class pdf_gop extends ModelePDFSuppliersOrders
   				$pdf->MultiCell(150, 0, $out,0,'L');
 
   				$pdf->SetFont('','', $default_font_size);
-  				$pdf->SetXY(40, 137.2);
+  				$pdf->SetXY(40, 137);
   				$out = $outputlangs->convToOutputCharset($object->lines['0']->desc);
   				$pdf->MultiCell(150, 16.5, $out,0,'L');
 
   				$pdf->SetFont('','', $default_font_size + 0.3);
-  				$pdf->SetXY(95.5, 154.45);
+  				$pdf->SetXY(95.5, 154.25);
   				$out = '<b>' . $outputlangs->convToOutputCharset(' : ' . price($object->total_ht). ' € Hors Taxes') .'</b>';
   				$pdf->writeHTML ($out);
 
   				$pdf->SetFont('','', $default_font_size);
-  				$pdf->SetXY(126.7, 195.7);
+  				$pdf->SetXY(126.7, 195.5);
   				if(!empty($object->cond_reglement_id)){
   					$text = $object->cond_reglement;
   				}else{
