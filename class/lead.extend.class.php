cd0970144062dfcf8d2b6c0f667041a52f3174d2
moreauf <moreauf@NGT15.theobald-groupe.local>
2021-02-05 17:28:32 +0100

    debug

--- class/lead.extend.class.php
+++ class/lead.extend.class.php
@@ -621,8 +621,8 @@ class Leadext extends Lead
 			$sql .= ' ' . $this->db->plimit($limit+1, $offset);
 		}
 		$this->business = array();
-        Var_dump($sql);
-        exit;
+        //Var_dump($sql);
+        //exit;
 		$resql = $this->db->query($sql);
 		if ($resql) {
 			$num = $this->db->num_rows($resql);
